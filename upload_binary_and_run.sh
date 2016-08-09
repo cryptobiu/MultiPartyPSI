ce6f92c2307a4a1e8020bf7454ace082aff86682
Naor Matania <naor9991@walla.co.il>
2016-08-09 06:14:17 -0700

    fix script

--- upload_binary_and_run.sh
+++ upload_binary_and_run.sh
@@ -1,3 +1,3 @@
-sshpass -p "305151094" scp naor@cybhead1.lnx.biu.ac.il:Config ./Config
-sshpass -p "305151094" scp naor@cybhead1.lnx.biu.ac.il:bin/MultiPartyPSI ./MultiPartyPSI
+sshpass -p "305151094" scp naor@cybhead1.lnx.biu.ac.il:Config MultiPartyPSI/Config
+sshpass -p "305151094" scp naor@cybhead1.lnx.biu.ac.il:bin/MultiPartyPSI MultiPartyPSI/MultiPartyPSI
 ./MultiPartyPSI $1 $2 $3
