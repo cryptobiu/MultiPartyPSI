46174b23fafe539556979d110f3a2a341b276c9c
Naor Matania <naor9991@walla.co.il>
2016-08-09 05:48:00 -0700

    add script

---
+++ upload_binary_and_run.sh
@@ -0,0 +1,3 @@
+sshpass -p "305151094" scp naor@cybhead1.lnx.biu.ac.il:Config ./Config
+sshpass -p "305151094" scp naor@cybhead1.lnx.biu.ac.il:bin/MultiPartyPSI ./MultiPartyPSI
+./MultiPartyPSI $1 $2 $3
