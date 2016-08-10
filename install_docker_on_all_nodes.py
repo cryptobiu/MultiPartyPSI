96dfd8a3f8a3b43e8dc3a9434cdee19ed22782aa
Naor Matania <naor9991@walla.co.il>
2016-08-10 04:26:36 -0700

    install

--- install_docker_on_all_nodes.py
+++ install_docker_on_all_nodes.py
@@ -11,4 +11,4 @@ numOfParties = int(config.get("General", "numOfParties"))
 for i in xrange(1,numOfParties+1):
     name = config.get(str(i), "name")
     os.system('sshpass -p "305151094" scp ./Dockerfile naor@{0}:Dockerfile'.format(name))
-    os.system('sshpass -p "305151094" scp ./install_docker_on_all_nodes.py naor@{0}:install_docker_on_all_nodes.py'.format(name))
+    os.system('sshpass -p "305151094" scp ./prepare_docker.sh naor@{0}:prepare_docker.sh'.format(name))
