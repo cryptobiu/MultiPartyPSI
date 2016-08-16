30296f0b41dc8b5d3e939689b7280d54516a58ba
Naor Matania <naor9991@walla.co.il>
2016-08-16 10:52:33 -0700

    Should work now

--- runner.py
+++ runner.py
@@ -101,6 +101,11 @@ def runMPPSI(strategy):
             ip = config.get(str(i), "ip")
 
             os.system('scp -i key.pem ./bin/MultiPartyPSI {0}:MultiPartyPSI/MultiPartyPSI'.format(ip))
+            os.system('ssh -i key.pem {0} "git pull MultiPartyPSI"'.format(ip))
+
+        for i in xrange(1,numOfParties+1):
+            ip = config.get(str(i), "ip")
+
             command_line = './MultiPartyPSI/MultiPartyPSI ' + str(i) + ' MultiPartyPSI/Config ' + str(PROGRAM_TYPE)
             print command_line
             os.system('ssh -i key.pem {0} "{1}" &'.format(ip, command_line))
