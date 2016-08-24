868a875af87eb437d233f1ddcdf1018c52cdd4b4
Naor Matania <naor9991@walla.co.il>
2016-08-24 05:38:10 -0700

    take all the experiment utils to another file

--- experiment_2.py
+++ experiment_2.py
@@ -7,6 +7,7 @@ import csv
 import os
 import ConfigParser
 import io
+import experiment_utils
 
 #SET_SIZES = [2**10,2**12,2**14,2**16,2**18,2**20]
 SET_SIZES = [2**2,2**4,2**6,2**8,2**10]
@@ -18,73 +19,18 @@ NUM_OF_PARTIES = 5
 # tc qdisc add dev eth0 root netem delay 100ms
 # tc qdisc change dev eth0 root netem loss 0.1%
 
-os.system('cmake -DCMAKE_BUILD_TYPE=Release CMakeLists.txt; make')
-process = subprocess.Popen(['git','rev-parse','HEAD'],stdout=subprocess.PIPE)
-REV = process.communicate()[0].rstrip('\n')
+experiment_utils.prepare_machines(NUM_OF_PARTIES)
 
-if not os.path.isfile('experiment1.csv'):
-    with open('experiment1.csv', 'wb') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=' ',
-                               quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csvwriter.writerow(['rev', 'start_time', 'key_size', 'num_parties', 'set_size', 'old_method','strategy', 'result'])
-else:
-    answer = raw_input("experiment1.csv exists. continuing means it would be appended. do you wish to continue ? (y/n)")
-    if answer=='n':
-        exit(0)
-
-def run_and_add_to_csv(key_size,set_size,old_method,strategy):
-    start_time = time.time()
-    result = runner.main(key_size=key_size,num_parties=NUM_OF_PARTIES,set_size=set_size,old_method=old_method,strategy=strategy)
-    result_str = '|'.join([str(item[1]) for item in sorted(result.items(),key= lambda x:x[0])])
-    row = [REV,str(start_time),str(key_size),str(NUM_OF_PARTIES),str(set_size),str(old_method),runner.getStrategyName(strategy),result_str]
-
-    with open('experiment1.csv', 'ab') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csvwriter.writerow(row)
-
-conf = open("BaseConfig", "rb").read()
-config = ConfigParser.RawConfigParser(allow_no_value=True)
-config.readfp(io.BytesIO(conf))
-
-if config.get("General", "remote") == "True":
-    for i in xrange(1,int(NUM_OF_PARTIES+1)):
-        ip = config.get(str(i), "ip")
-        os.system('ssh -i key.pem {0} "cd MultiPartyPSI; git pull"'.format(ip))
-        os.system('scp -i key.pem ./bin/MultiPartyPSI {0}:MultiPartyPSI/MultiPartyPSI'.format(ip))
+experiment_utils.prepare_results_file('experiment2.csv')
 
 for set_size in SET_SIZES:
     for key_size in KEY_SIZES:
         for strategy in STRATEGIES:
             for i in xrange(10):
-                run_and_add_to_csv(key_size,set_size,False,strategy)
+                experiment_utils.run_and_add_to_csv('experiment2.csv',NUM_OF_PARTIES,key_size,set_size,False,strategy)
 for set_size in SET_SIZES:
     for key_size in KEY_SIZES:
         for i in xrange(10):
-            run_and_add_to_csv(key_size,set_size,True,None)
+            experiment_utils.run_and_add_to_csv('experiment2.csv',key_size,NUM_OF_PARTIES,set_size,True,None)
 
-with open('experiment1_avg.csv', 'wb') as csvf:
-    csvwriter = csv.writer(csvf, delimiter=' ',
-                           quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    csvwriter.writerow(['rev', 'start_time', 'key_size', 'num_parties', 'set_size', 'old_method','strategy', 'result'])
-    with open('experiment1.csv', 'rb') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        csvreader.next() # remove title
-        try:
-            while True:
-                rows = [csvreader.next() for i in xrange(10)]
-                results = [(0,0)]*NUM_OF_PARTIES
-                for i in xrange(10):
-                    assert rows[i][0] == rows[0][0] #rev
-                    assert rows[i][2] == rows[0][2] #key_size
-                    assert rows[i][3] == rows[0][3] #num_parties
-                    assert rows[i][4] == rows[0][4] #set_size
-                    assert rows[i][5] == rows[0][5] #old_method
-                    assert rows[i][6] == rows[0][6] #strategy
-                    times_and_bytes = [eval(x) for x in rows[i][7].split('|')]
-                    results = [(results[i][0]+times_and_bytes[i][0],results[i][1]+times_and_bytes[i][1]) for i in xrange(NUM_OF_PARTIES)]
-                results = map(lambda x: str((x[0]/10,x[1]/10)), results)
-                rows[0][7]='|'.join(results)
-                csvwriter.writerow(rows[0])
-        except StopIteration:
-            print "done !"
+experiment_utils.avg_experiments('experiment2.csv', 'experiment2_avg.csv', NUM_OF_PARTIES)
---
+++ experiment_utils.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import runner
+import csv
+import time
+import subprocess
+import os
+import ConfigParser
+import io
+
+os.system('cmake -DCMAKE_BUILD_TYPE=Release CMakeLists.txt; make')
+process = subprocess.Popen(['git','rev-parse','HEAD'],stdout=subprocess.PIPE)
+REV = process.communicate()[0].rstrip('\n')
+
+def prepare_machines(num_of_parties):
+    conf = open("BaseConfig", "rb").read()
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.readfp(io.BytesIO(conf))
+
+    if config.get("General", "remote") == "True":
+        for i in xrange(1,int(num_of_parties+1)):
+            ip = config.get(str(i), "ip")
+            os.system('ssh -i key.pem {0} "cd MultiPartyPSI; git pull"'.format(ip))
+            os.system('scp -i key.pem ./bin/MultiPartyPSI {0}:MultiPartyPSI/MultiPartyPSI'.format(ip))
+
+def run_and_add_to_csv(results_file_path,num_of_parties,key_size,set_size,old_method,strategy,bandwidth=None,delay=None):
+    start_time = time.time()
+    result = runner.main(key_size=key_size,num_parties=num_of_parties,set_size=set_size,old_method=old_method,strategy=strategy)
+    result_str = '|'.join([str(item[1]) for item in sorted(result.items(),key= lambda x:x[0])])
+    row = [REV,str(bandwidth),str(delay),str(start_time),str(key_size),str(num_of_parties),str(set_size),str(old_method),runner.getStrategyName(strategy),result_str]
+
+    with open(results_file_path, 'ab') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=' ',
+                               quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(row)
+
+def prepare_results_file(config_file_path):
+    if not os.path.isfile(config_file_path):
+        with open(config_file_path, 'wb') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=' ',
+                                   quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            csvwriter.writerow(['rev', 'bandwidth', 'delay', 'start_time', 'key_size', 'num_parties', 'set_size', 'old_method','strategy', 'result'])
+    else:
+        answer = raw_input("{0} exists. continuing means it would be appended. do you wish to continue ? (y/n)".format(config_file_path))
+        if answer=='n':
+            exit(0)
+
+def avg_experiments(result_file_path, avg_result_file_path, num_of_parties):
+    with open(avg_result_file_path, 'wb') as csvf:
+        csvwriter = csv.writer(csvf, delimiter=' ',
+                               quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(['rev', 'bandwidth', 'delay', 'start_time', 'key_size', 'num_parties', 'set_size', 'old_method','strategy', 'result'])
+        with open(result_file_path, 'rb') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            csvreader.next() # remove title
+            try:
+                while True:
+                    rows = [csvreader.next() for i in xrange(10)]
+                    results = [(0,0)]*num_of_parties
+                    for i in xrange(10):
+                        assert rows[i][0] == rows[0][0] #rev
+                        assert rows[i][1] == rows[0][1] #bandwidth
+                        assert rows[i][2] == rows[0][2] #delay
+
+                        assert rows[i][2] == rows[0][4] #key_size
+                        assert rows[i][3] == rows[0][5] #num_parties
+                        assert rows[i][4] == rows[0][6] #set_size
+                        assert rows[i][5] == rows[0][7] #old_method
+                        assert rows[i][6] == rows[0][8] #strategy
+                        times_and_bytes = [eval(x) for x in rows[i][9].split('|')]
+                        results = [(results[i][0]+times_and_bytes[i][0],results[i][1]+times_and_bytes[i][1]) for i in xrange(NUM_OF_PARTIES)]
+                    results = map(lambda x: str((x[0]/10,x[1]/10)), results)
+                    rows[0][9]='|'.join(results)
+                    csvwriter.writerow(rows[0])
+            except StopIteration:
+                print "done !"
