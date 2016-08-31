1d3e3d5f56b9e9f3bf11ab2905d3d6e410b27d5e
Naor Matania <naor9991@walla.co.il>
2016-08-31 02:10:32 -0700

    run on real set sizes

--- experiment_1.py
+++ experiment_1.py
@@ -9,8 +9,7 @@ import ConfigParser
 import io
 import experiment_utils
 
-#SET_SIZES = [2**10,2**12,2**14,2**16,2**18,2**20]
-SET_SIZES = [2**2,2**4,2**6,2**8,2**10]
+SET_SIZES = [2**10,2**12,2**14,2**16,2**18,2**20]
 KEY_SIZES = [80,128]
 STRATEGIES = [runner.Strategy.POLYNOMIALS, runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH]
 # and ofcourse kissner
@@ -25,9 +24,12 @@ for set_size in SET_SIZES:
         for strategy in STRATEGIES:
             for i in xrange(10):
                 experiment_utils.run_and_add_to_csv('experiment1.csv',NUM_OF_PARTIES,key_size,set_size,False,strategy)
+'''
+don't run kissner any more !!!!!
 for set_size in SET_SIZES:
     for key_size in KEY_SIZES:
         for i in xrange(10):
             experiment_utils.run_and_add_to_csv('experiment1.csv',NUM_OF_PARTIES,key_size,set_size,True,None)
+'''
 
 experiment_utils.avg_experiments('experiment1.csv', 'experiment1_avg.csv', NUM_OF_PARTIES)
