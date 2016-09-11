bd56e392500bbd9fce8805b6eb6cdeec78a2e24f
Naor Matania <naor9991@walla.co.il>
2016-09-11 19:41:53 +0300

    add results for experiment 3

--- make_table_3.py
+++ make_table_3.py
@@ -6,12 +6,12 @@ import sys
 import math
 
 place = {'BLOOM_FILTER' : 1, 'POLYNOMIALS_SIMPLE_HASH' : 2, 'GAUSS_SIMPLE_HASH' : 3}
-BANDWIDTH_AND_LATENCY = [('1000mbit','0.2ms'),('54mbit','0.2ms'),('25mbit','10ms'),('10mbit','50ms'),('3.6mbit','500ms')]
 
 KEY_SIZE = 80
 dir_name = sys.argv[1]
+NUM_OF_PARTIES = [3,5,7,10]
 
-with open("{0}/experiment2_avg.csv".format(dir_name), 'rb') as csvfile:
+with open("{0}/experiment3_avg.csv".format(dir_name), 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     Result = namedtuple('Result',csvreader.next())
     results = []
@@ -27,29 +27,29 @@ with open("{0}/experiment2_avg.csv".format(dir_name), 'rb') as csvfile:
     res = {'BLOOM_FILTER' : {}, 'POLYNOMIALS_SIMPLE_HASH' : {}, 'GAUSS_SIMPLE_HASH' : {}}
     params = []
 
-    for bandwidth, latency in BANDWIDTH_AND_LATENCY:
-        row = filter(lambda x: x.bandwidth == bandwidth and x.latency == latency,results)
+    for num_of_parties in NUM_OF_PARTIES:
+        row = filter(lambda x: x.num_parties == str(num_of_parties),results)
         
         time_vals = dict(map(lambda x: (x.strategy, "%.2f" % eval(x.result.split('|')[0])[0]), row))
 
         for strategy in res.keys():
-            res[strategy][(bandwidth,latency)] = time_vals[strategy]
+            res[strategy][num_of_parties] = time_vals[strategy]
     
-    with open("{0}/experiment2.txt".format(dir_name), 'wb') as f:
+    with open("{0}/experiment3.txt".format(dir_name), 'wb') as f:
 
         f.write('\\begin{table*}[t]\n')
         f.write('\\centering\n')
         f.write('\\begin{tabular}{| l | l | l | l | l | l |}\n')
         f.write('\\hline\n')
-        f.write('Bandwidth, Latency & ' + ' & '.join(["{0}, {1}".format(bandwidth, latency) for bandwidth, latency in BANDWIDTH_AND_LATENCY]) + " \\\\\n")
+        f.write('Num of parties & ' + ' & '.join(["{0}".format(num_of_parties) for num_of_parties in NUM_OF_PARTIES]) + " \\\\\n")
         
         f.write('\\hline\n')
         f.write('\\hline\n')
         
         for strategy in res.keys():
-            f.write(" & ".join([strategy.replace('_',' ')]+[res[strategy][(bandwidth,latency)] for bandwidth, latency in BANDWIDTH_AND_LATENCY]) + " \\\\\\hline\n")
+            f.write(" & ".join([strategy.replace('_',' ')]+[res[strategy][num_of_parties] for num_of_parties in NUM_OF_PARTIES]) + " \\\\\\hline\n")
         
         f.write('\\end{tabular}\n')
-        f.write('\\caption{Times (in seconds) for 5 parties}\n')
-        f.write('\\label{tab:results2}\n')
+        f.write('\\caption{Times (in seconds) for changing number of parties}\n')
+        f.write('\\label{tab:results3}\n')
         f.write('\\end{table*}\n')
