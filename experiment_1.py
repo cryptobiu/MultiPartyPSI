b62b5a48b0cdbf07f9b714f4b51e544f37864f57
Naor Matania <naor9991@walla.co.il>
2016-09-19 07:32:13 -0700

    add kissner to the experiment

--- experiment_1.py
+++ experiment_1.py
@@ -9,7 +9,7 @@ import ConfigParser
 import io
 import experiment_utils
 
-SET_SIZES = [2**10,2**12,2**14,2**16,2**18]
+SET_SIZES = [2**2,2**4,2**6,2**8,2**10,2**12,2**14,2**16,2**18]
 #KEY_SIZES = [80,128]
 KEY_SIZES = [80, 128]
 #STRATEGIES = [runner.Strategy.POLYNOMIALS, runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH]
@@ -19,6 +19,8 @@ STRATEGIES = [runner.Strategy.SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH,
 NUM_THREADS = [1,2,3,4,None]
 
 LIMITS = {runner.Strategy.SIMPLE_HASH : 2**14,}
+
+KISSNER_LIMIT=2**8
 
 # and ofcourse kissner
 NUM_OF_PARTIES = 5
@@ -31,12 +33,22 @@ for num_threads in NUM_THREADS:
     for set_size in SET_SIZES:
         for key_size in KEY_SIZES:
             for strategy in STRATEGIES:
-                if LIMITS.has_key(strategy) and LIMITS[strategy] > set_size:
+                if LIMITS.has_key(strategy) and set_size > LIMITS[strategy]:
                     continue
                 for i in xrange(10):
                     print "num_threads: {0}, set_size: {1}, key_size: {2}, strategy: {3}".format(
                         num_threads, set_size, key_size, strategy)
                     while not experiment_utils.run_and_add_to_csv('experiment1.csv',NUM_OF_PARTIES,key_size,set_size,False,strategy,num_threads=num_threads):
                         pass
+
+for set_size in SET_SIZES:
+    for key_size in KEY_SIZES:
+            if set_size > KISSNER_LIMIT:
+                continue
+            for i in xrange(10):
+                print "set_size: {0}, key_size: {1}, kissner_method".format(
+                    set_size, key_size)
+                while not experiment_utils.run_and_add_to_csv('experiment1.csv',NUM_OF_PARTIES,key_size,set_size,True,None):
+                    pass
 
 experiment_utils.avg_experiments('experiment1.csv', 'experiment1_avg.csv')
