8116c79c0c379565b88432f1f2c05714573c5e36
Naor Matania <naor9991@walla.co.il>
2016-09-19 08:01:51 -0700

    flow

--- experiment_1.py
+++ experiment_1.py
@@ -29,15 +29,24 @@ experiment_utils.prepare_machines(NUM_OF_PARTIES)
 
 experiment_utils.prepare_results_file('experiment1.csv')
 
+FLOW = False
+
 for num_threads in NUM_THREADS:
     for set_size in SET_SIZES:
         for key_size in KEY_SIZES:
             for strategy in STRATEGIES:
                 if LIMITS.has_key(strategy) and set_size > LIMITS[strategy]:
                     continue
+                print "num_threads: {0}, set_size: {1}, key_size: {2}, strategy: {3}".format(
+                    num_threads, set_size, key_size, strategy)
+                if not Flow:
+                    result = raw_input("skip (s), continue (c)")
+                    if result == 's':
+                        continue
+                    elif result == 'c':
+                        FLOW = True
                 for i in xrange(10):
-                    print "num_threads: {0}, set_size: {1}, key_size: {2}, strategy: {3}".format(
-                        num_threads, set_size, key_size, strategy)
+
                     print "start time: " + time.asctime()
                     while not experiment_utils.run_and_add_to_csv('experiment1.csv',NUM_OF_PARTIES,key_size,set_size,False,strategy,num_threads=num_threads):
                         pass
