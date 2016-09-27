1c07e24cce402fe0bc6904caefc12015655ab5d6
Naor Matania <naor9991@walla.co.il>
2016-09-27 04:34:18 -0700

    change experiment back

--- experiment_1.py
+++ experiment_1.py
@@ -10,9 +10,7 @@ import io
 import experiment_utils
 
 SET_SIZES = [2**2,2**4,2**6,2**8,2**10,2**12,2**14,2**16,2**18]
-#KEY_SIZES = [80,128]
 KEY_SIZES = [80, 128]
-#STRATEGIES = [runner.Strategy.POLYNOMIALS, runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH]
 STRATEGIES = [runner.Strategy.SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH,
               runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS,
               runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.TWO_PARTY]
@@ -30,7 +28,7 @@ experiment_utils.prepare_machines(NUM_OF_PARTIES)
 experiment_utils.prepare_results_file('experiment1.csv')
 
 FLOW = False
-'''
+
 for num_threads in NUM_THREADS:
     for set_size in SET_SIZES:
         for key_size in KEY_SIZES:
@@ -50,7 +48,7 @@ for num_threads in NUM_THREADS:
                     print "start time: " + time.asctime()
                     while not experiment_utils.run_and_add_to_csv('experiment1.csv',NUM_OF_PARTIES,key_size,set_size,False,strategy,num_threads=num_threads):
                         pass
-'''
+
 
 for set_size in SET_SIZES:
     for key_size in KEY_SIZES:
