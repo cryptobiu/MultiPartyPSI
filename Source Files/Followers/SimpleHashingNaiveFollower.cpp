43fd946ec915b1fcab47e6ec90fc14f4c2f89bf5
Naor Matania <naor9991@walla.co.il>
2016-09-27 04:27:13 -0700

    send in one chunk

--- Source Files/Followers/SimpleHashingNaiveFollower.cpp
+++ Source Files/Followers/SimpleHashingNaiveFollower.cpp
@@ -18,24 +18,22 @@ void SimpleHashingNaiveFollower::run() {
     PrgFromOpenSSLAES prg(ceil_divide(m_followerSet.m_numOfBins*m_followerSet.m_numOfHashFunctions*m_followerSet.m_maxBinSize*m_followerSet.m_maskSizeInBytes,16));
     prg.setKey(key);
 
+    vector<uint8_t> random;
+    prg.getPRGBytes(random, 0, m_followerSet.m_numOfBins*m_followerSet.m_numOfHashFunctions*m_followerSet.m_maxBinSize*m_followerSet.m_maskSizeInBytes);
+
     uint32_t progress = 0;
     for (uint32_t k=0; k < m_followerSet.m_numOfBins; k++) {
         uint32_t numOfElementsInBin = m_followerSet.m_numOfElementsInBin.get()[k];
         if (numOfElementsInBin > m_followerSet.m_numOfHashFunctions*m_followerSet.m_maxBinSize) {
             PRINT() << "ERROR MAX SIZE IN BIN IS NOT BIG ENOUGH !!!!";
         }
-        uint32_t numOfRandomBytes = (m_followerSet.m_numOfHashFunctions*m_followerSet.m_maxBinSize-numOfElementsInBin)*m_followerSet.m_maskSizeInBytes;
         //send the masks to the receiver
-        send_masks(m_followerSet.m_masks.get()+progress*m_followerSet.m_maskSizeInBytes, numOfElementsInBin, m_followerSet.m_maskSizeInBytes, m_leader);
-
-        if (numOfRandomBytes != 0) {
-            vector<uint8_t> random;
-            prg.getPRGBytes(random, 0, numOfRandomBytes);
-
-            send_masks(random.data(), m_followerSet.m_numOfHashFunctions*m_followerSet.m_maxBinSize-numOfElementsInBin, m_followerSet.m_maskSizeInBytes, m_leader);
-        }
+        memcpy(random.data()+ k*m_followerSet.m_numOfHashFunctions*m_followerSet.m_maxBinSize*m_followerSet.m_maskSizeInBytes,
+               m_followerSet.m_masks.get()+progress*m_followerSet.m_maskSizeInBytes, numOfElementsInBin*m_followerSet.m_maskSizeInBytes);
 
         progress = progress + numOfElementsInBin;
     }
+
+    send_masks(random.data(), m_followerSet.m_numOfBins*m_followerSet.m_numOfHashFunctions*m_followerSet.m_maxBinSize, m_followerSet.m_maskSizeInBytes, m_leader);
 
 }
