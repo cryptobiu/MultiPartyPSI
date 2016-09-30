f80811f60234c51679ee938532933e7798f3d245
Naor Matania <naor9991@walla.co.il>
2016-09-30 05:43:18 -0700

    table utils

--- make_table_3.py
+++ make_table_3.py
@@ -4,6 +4,7 @@ import csv
 from collections import namedtuple
 import sys
 import math
+import table_utils
 
 place = {'BLOOM_FILTER' : 1, 'POLYNOMIALS_SIMPLE_HASH' : 2, 'GAUSS_SIMPLE_HASH' : 3}
 
@@ -12,16 +13,8 @@ dir_name = sys.argv[1]
 NUM_OF_PARTIES = [3,5,7,10]
 
 with open("{0}/experiment3_avg.csv".format(dir_name), 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    Result = namedtuple('Result',csvreader.next())
-    results = []
-    
-    try:
-        while True:
-            row = csvreader.next()
-            results.append(Result(*row))
-    except StopIteration:
-        print("done !")
+    results = table_utils.readRows(csvfile)
+
     results = filter(lambda x: x.key_size==str(KEY_SIZE),results)
     
     res = {'BLOOM_FILTER' : {}, 'POLYNOMIALS_SIMPLE_HASH' : {}, 'GAUSS_SIMPLE_HASH' : {}}
@@ -29,8 +22,8 @@ with open("{0}/experiment3_avg.csv".format(dir_name), 'rb') as csvfile:
 
     for num_of_parties in NUM_OF_PARTIES:
         row = filter(lambda x: x.num_parties == str(num_of_parties),results)
-        
-        time_vals = dict(map(lambda x: (x.strategy, "%.2f" % eval(x.result.split('|')[0])[0]), row))
+
+        time_vals = dict(map(lambda x: (x.strategy, "%.2f" % eval(x.result1.split('|')[0])), row))
 
         for strategy in res.keys():
             res[strategy][num_of_parties] = time_vals[strategy]
