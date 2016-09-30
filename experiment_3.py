09ff556c367bc8bf9bc0f050dc9ad17ad0e2ff90
Naor Matania <naor9991@walla.co.il>
2016-09-30 08:03:20 -0700

    add

--- experiment_3.py
+++ experiment_3.py
@@ -14,7 +14,7 @@ KEY_SIZE = 80
 STRATEGIES = [runner.Strategy.SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH,
               runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS,
               runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.TWO_PARTY]
-NUM_OF_PARTIES = [2,4,6,8,10]
+NUM_OF_PARTIES = [2,3,5,7,9,10]
 
 experiment_utils.prepare_machines(NUM_OF_PARTIES[-1])
 
