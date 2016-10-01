3902f2ae05488a80da6bc3e0b243098f283de3df
Naor Matania <naor9991@walla.co.il>
2016-10-01 10:34:08 -0700

    return most of the stuff

--- experiment_3.py
+++ experiment_3.py
@@ -17,11 +17,12 @@ STRATEGIES = [runner.Strategy.SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH,
               runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.TWO_PARTY]
 '''
 
-STRATEGIES = [runner.Strategy.POLYNOMIALS,
+STRATEGIES = [runner.Strategy.GAUSS_SIMPLE_HASH,
+              runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS,
               runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.TWO_PARTY]
 
 #NUM_OF_PARTIES = [2,3,5,7,9,10]
-NUM_OF_PARTIES = [7,9,10]
+NUM_OF_PARTIES = [9,10]
 
 experiment_utils.prepare_machines(NUM_OF_PARTIES[-1])
 
@@ -29,7 +30,7 @@ experiment_utils.prepare_results_file('experiment3.csv')
 
 for num_of_parties in NUM_OF_PARTIES:
     for strategy in STRATEGIES:
-        for i in xrange(7):
+        for i in xrange(10):
             while not experiment_utils.run_and_add_to_csv('experiment3.csv',num_of_parties,KEY_SIZE,SET_SIZE,False,strategy,num_threads=4):
                 pass
 
