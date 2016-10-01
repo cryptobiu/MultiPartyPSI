7fccb47472e91030e9ccd4455bd6b551e93e04c2
Naor Matania <naor9991@walla.co.il>
2016-10-01 05:06:23 -0700

    less to check

--- experiment_3.py
+++ experiment_3.py
@@ -11,10 +11,18 @@ import experiment_utils
 
 SET_SIZE = 2**16
 KEY_SIZE = 80
+'''
 STRATEGIES = [runner.Strategy.SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH,
               runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS,
               runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.TWO_PARTY]
-NUM_OF_PARTIES = [2,3,5,7,9,10]
+'''
+
+STRATEGIES = [runner.Strategy.GAUSS_SIMPLE_HASH,
+              runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS,
+              runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.TWO_PARTY]
+
+#NUM_OF_PARTIES = [2,3,5,7,9,10]
+NUM_OF_PARTIES = [7,9,10]
 
 experiment_utils.prepare_machines(NUM_OF_PARTIES[-1])
 
