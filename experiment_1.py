cc7ad5a2249f9e2e165a49090214bf74c863ad21
Naor Matania <naor9991@walla.co.il>
2017-01-26 12:21:05 -0800

    change

--- experiment_1.py
+++ experiment_1.py
@@ -9,7 +9,7 @@ import ConfigParser
 import io
 import experiment_utils
 
-SET_SIZES = [2**8,2**10,2**12,2**14,2**16,2**18]
+SET_SIZES = [2**20]
 
 KEY_SIZES = [80, 128]
 
@@ -18,7 +18,7 @@ STRATEGIES = [runner.Strategy.SIMPLE_HASH, runner.Strategy.GAUSS_SIMPLE_HASH,
               runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS,
               runner.Strategy.POLYNOMIALS_SIMPLE_HASH, runner.Strategy.TWO_PARTY]
 '''
-STRATEGIES = [runner.Strategy.POLYNOMIALS_SIMPLE_HASH]
+STRATEGIES = [runner.Strategy.BLOOM_FILTER, runner.Strategy.POLYNOMIALS_SIMPLE_HASH]
 NUM_THREADS = [1,2,4,None]
 
 LIMITS = {runner.Strategy.POLYNOMIALS : 2**16}
